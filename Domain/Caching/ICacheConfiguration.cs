1085672aecfce401ebcb6475b01167defe7664b1
slsniffer <sl.sniffer@gmail.com>
2017-06-09 11:11:58 +0300

    - add possibility to disable cache

---
+++ Domain/Caching/ICacheConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamSL.Infrastructure.Domain.Caching
+{
+    public interface ICacheConfiguration
+    {
+        bool IsEnabled { get; }
+    }
+}
--- Domain/Caching/MemoryCacheStorage.cs
+++ Domain/Caching/MemoryCacheStorage.cs
@@ -5,23 +5,35 @@ namespace TeamSL.Infrastructure.Domain.Caching
 {
     public class MemoryCacheStorage : ICacheStorage
     {
+        private readonly ICacheConfiguration _configuration;
         private static readonly MemoryCache _cache = new MemoryCache("cache");
+
+        public MemoryCacheStorage(ICacheConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public T Get<T>(string key)
         {
-            return (T) _cache.Get(key);
+            if (!_configuration.IsEnabled)
+                return default(T);
+
+            return (T)_cache.Get(key);
         }
 
         public bool TryGet<T>(string key, out T value)
         {
             value = default(T);
 
+            if (!_configuration.IsEnabled)
+                return false;
+
             if (_cache.Contains(key))
             {
                 object existingValue = _cache.Get(key);
                 if (existingValue != null)
                 {
-                    value = (T) existingValue;
+                    value = (T)existingValue;
                     return true;
                 }
             }
@@ -36,27 +48,32 @@ namespace TeamSL.Infrastructure.Domain.Caching
 
         public void Set<T>(string key, T value)
         {
-            Set(key, value, DateTime.Now.AddMinutes(30));
+            if (_configuration.IsEnabled)
+                Set(key, value, DateTime.Now.AddMinutes(30));
         }
 
         public void Set<T>(string key, T value, DateTime absoluteExpiration)
         {
-            SetInternal(key, value, new CacheItemPolicy { AbsoluteExpiration = absoluteExpiration });
+            if (_configuration.IsEnabled)
+                SetInternal(key, value, new CacheItemPolicy { AbsoluteExpiration = absoluteExpiration });
         }
 
         public void Set<T>(string key, T value, TimeSpan slidingExpiration)
         {
-            SetInternal(key, value, new CacheItemPolicy {SlidingExpiration = slidingExpiration});
+            if (_configuration.IsEnabled)
+                SetInternal(key, value, new CacheItemPolicy { SlidingExpiration = slidingExpiration });
         }
 
         private void SetInternal<T>(string key, T value, CacheItemPolicy policy)
         {
-            _cache.Add(key, value, policy);
+            if (_configuration.IsEnabled)
+                _cache.Add(key, value, policy);
         }
 
         public void Remove(string key)
         {
-            _cache.Remove(key);
+            if (_configuration.IsEnabled)
+                _cache.Remove(key);
         }
     }
 }
