935b608ba790f73772f8ca9d9419558fe23b3ffc
sniffer <sl.sniffer@gmail.com>
2018-03-21 12:15:21 +0300

    - added string extensions

--- Utils/Extensions/StringExtensions.cs
+++ Utils/Extensions/StringExtensions.cs
@@ -1,6 +1,32 @@
-﻿namespace TeamSL.Infrastructure.Utils.Extensions
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TeamSL.Infrastructure.Utils.Extensions
 {
     public static class StringExtensions
     {
+        public static string ToBase64(this string value)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+        }
+
+        public static string FromBase64(this string value)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(value));
+        }
+
+        public static string AsMd5Hash(this string str)
+        {
+            var hash = new StringBuilder();
+            var md5Provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5Provider.ComputeHash(new UTF8Encoding().GetBytes(str));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
     }
 }
