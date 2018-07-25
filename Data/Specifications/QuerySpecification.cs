11c3180dd5e0a07dee9c3cf3a0d759abb23b8417
sniffer <sl.sniffer@gmail.com>
2018-07-25 10:29:59 +0300

    - fix data nhibernate

--- Data/Specifications/QuerySpecification.cs
+++ Data/Specifications/QuerySpecification.cs
@@ -4,11 +4,6 @@ using System.Linq.Expressions;
 
 namespace TeamSL.Infrastructure.Data.Specifications
 {
-    public interface IQuerySpecification<T>
-    {
-        IQueryable<T> SatisfyingElementsFrom(IQueryable<T> candidates);
-    }
-
     public abstract class QuerySpecification<T> : IQuerySpecification<T>
     {
         protected abstract Expression<Func<T, bool>> MatchingCriteria();
