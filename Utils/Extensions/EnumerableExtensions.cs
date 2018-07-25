8913139a3473c41dfde3b80a7b810acdff432abf
sniffer <sl.sniffer@gmail.com>
2018-07-25 10:13:50 +0300

    - added extensions.

--- Utils/Extensions/EnumerableExtensions.cs
+++ Utils/Extensions/EnumerableExtensions.cs
@@ -26,5 +26,14 @@ namespace TeamSL.Infrastructure.Utils.Extensions
 
             return source;
         }
+
+        public static IEnumerable<IEnumerable<T>> ChunkBy<T>(this IEnumerable<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
     }
 }
